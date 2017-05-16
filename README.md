da71c31e8ea7d239b7a99cb51adf185b8e5f1ae0
YlcDev <yannicklc.dev@gmail.com>
2017-05-16 18:28:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+LareaNetwork
+============
+
+A Symfony project created on May 13, 2017, 12:34 pm.
---
+++ src/LBM/FrontPageBundle/FrontPageBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LBM\FrontPageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FrontPageBundle extends Bundle
+{
+}
